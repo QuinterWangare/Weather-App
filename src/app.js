fdd9fbc583d91ff3e89704d1409436e6d95a49ba
Quinter Wangare <quinterwangarem@gmail.com>
2023-12-09 18:28:10 +0300

    Injected more weather data

--- src/app.js
+++ src/app.js
@@ -2,8 +2,40 @@ function updateWeather(response) {
   let temperatureElement = document.querySelector("#temperature-degree-value");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let humidityElement = document.querySelector("#humidity-value");
+  let windSpeed = document.querySelector("#wind-speed");
+  let weatherDescription = document.querySelector("#weather-description");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
+  timeElement.innerHTML = formatDate(date);
+  weatherDescription.innerHTML = response.data.condition.description;
+  windSpeed.innerHTML = `${response.data.wind.speed}km/h`;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+  let hour = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hour}:${minutes}`;
 }
 
 function searchCity(city) {
